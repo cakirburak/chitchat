8d1aa2b57832a5ce79e3a8dcc7a85f235aa60848
cakirburak <cakirburak43@hotmail.com>
2024-02-10 23:41:46 +0300

    get user messages api is implemented

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -1,6 +1,22 @@
 import Conversation from "../models/conversation.model.js"
 import Message from "../models/message.mode.js"
 
+export const getMessages = async (req, res) => {
+	try {
+		const { id: userToChatId } = req.params
+		const senderId = req.user.userId
+
+		const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] }
+		}).populate("messages")
+
+		res.status(200).json(conversation.messages || { "hello": "hello" })
+	} catch (error) {
+		console.log("Error in getMessages controller: ", error.message);
+		res.status(500).json({ error: "Internal Server Error" })
+	}
+}
+
 export const sendMessage = async (req, res) => {
 	try {
 		const { message } = req.body
