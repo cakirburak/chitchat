4d599eb0f719ef619d57808f69991aefac898308
cakirburak <cakirburak43@hotmail.com>
2024-02-12 18:03:55 +0300

    Sidebar component for Home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,11 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+
 const Home = () => {
 	return (
-		<div>Home</div>
+		<div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-30">
+			<Sidebar />
+			{/* <MessageContainer /> */}
+		</div>
 	)
 }
 
