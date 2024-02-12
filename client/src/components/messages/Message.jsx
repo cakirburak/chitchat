80d44a5ab7e93d4f43f8dde098cf6dec4f9883f8
cakirburak <cakirburak43@hotmail.com>
2024-02-13 02:03:14 +0300

    messages UI integrated with get messages api

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -1,13 +1,25 @@
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext"
+import { useConversationContext } from "../../context/ConversationContext"
+
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext()
+	const { selectedConversation } = useConversationContext()
+
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = message.createdAt.slice(11, 16);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profileImg = fromMe ? authUser.profileImg : selectedConversation?.profileImg;
+	const bubbleBgColor = fromMe ? "bg-yellow-500" : "";
+
 	return (
-		<div className="chat chat-end">
-			<div className="chat-image avatar">
-				<div className="w-10 rounded-full">
-					<img src="https://avatar.iran.liara.run/public/boy?username=usertest1" alt="user avatar" />
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profileImg} />
 				</div>
 			</div>
-			<div className="chat-bubble text-white bg-yellow-500">How you doin!</div>
-			<div className="chat-footer opacity-50 text-xs flex gap-1 items-center">15:32</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
 		</div>
 	)
 }
--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,13 +1,19 @@
+import useGetMessages from "../../hooks/useGetMessages"
+import MessageSkeleton from "../skeletons/MessageSkeleton"
 import Message from "./Message"
 
 const Messages = () => {
+	const { loading, messages } = useGetMessages()
+
 	return (
 		<div className="px-4 flex-1 overflow-auto">
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
+			{loading && [...Array(3)].map((_, index) => (<MessageSkeleton key={index} />))}
+			{!loading && messages.length === 0 && (
+				<p className="text-center">Send a message to start the conversation</p>
+			)}
+			{!loading && messages.length > 0 && messages.map((message, index) => (
+				<Message key={index} message={message} />
+			))}
 		</div>
 	)
 }
