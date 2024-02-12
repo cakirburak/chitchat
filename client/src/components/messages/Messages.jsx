107568b84c287c2eeb0a1a5a537655c69cb92710
cakirburak <cakirburak43@hotmail.com>
2024-02-13 02:24:57 +0300

    scroll down behavior for masseges UI

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react"
 import useGetMessages from "../../hooks/useGetMessages"
 import MessageSkeleton from "../skeletons/MessageSkeleton"
 import Message from "./Message"
@@ -5,14 +6,24 @@ import Message from "./Message"
 const Messages = () => {
 	const { loading, messages } = useGetMessages()
 
+	const lastMessageRef = useRef();
+
+	useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
+
 	return (
 		<div className="px-4 flex-1 overflow-auto">
 			{loading && [...Array(3)].map((_, index) => (<MessageSkeleton key={index} />))}
 			{!loading && messages.length === 0 && (
 				<p className="text-center">Send a message to start the conversation</p>
 			)}
-			{!loading && messages.length > 0 && messages.map((message, index) => (
-				<Message key={index} message={message} />
+			{!loading && messages.length > 0 && messages.map((message) => (
+				<div key={message._id} ref={lastMessageRef}>
+					<Message message={message} />
+				</div>
 			))}
 		</div>
 	)
